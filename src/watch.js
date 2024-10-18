a787aca00e2b7accaf2aab98c2524d23ec6c9867
Kunkka Huang <huangkunkunkun@gmail.com>
2024-10-18 15:44:40 +0800

    Update watch.js

--- src/watch.js
+++ src/watch.js
@@ -1,6 +1,6 @@
 const watchboy = require('watchboy');
 
-const PATTERNS = ['**/*', '!node_modules', '!node_modules/**/*', '!.*', '!**/*.map'];
+const PATTERNS = [];//['**/*', '!node_modules', '!node_modules/**/*', '!.*', '!**/*.map'];
 
 module.exports = ({ root, patterns }) => {
   const watcher = watchboy([...PATTERNS, ...patterns.map(s => `${s}`)], {
